d960aa969fc0ae5c6d2349bffe581322060854a6
Josh Mock <josh@joshmock.com>
2025-08-12 10:44:45 -0500

    try switching broken test to proper async for easier debugging

--- test/inflight-and-close.js
+++ test/inflight-and-close.js
@@ -39,3 +39,37 @@ test('inflight and close', async (t) => {
   })
   await t.completed
 })
+
+test('inflight and close async', async (t) => {
+  t = tspl(t, { plan: 3 })
+
+  const server = http.createServer({ joinDuplicateHeaders: true }, (_req, res) => {
+    console.log('async: request received')
+    res.writeHead(200)
+    res.end('Response body')
+    res.socket.end() // Close the connection immediately with every response
+  }).listen(0, '127.0.0.1', async function () {
+    const url = `http://127.0.0.1:${this.address().port}`
+
+    try {
+      const { body: body1 } = await request(url)
+      console.log('async first response')
+      t.ok(true, 'first response')
+      body1.resume()
+      body1.on('close', () => t.ok(true, 'first body closed'))
+
+      const { body: body2 } = await request(url)
+      console.log('async second response')
+      t.ok(true, 'second response')
+      body2.resume()
+      body2.on('close', () => {
+        console.log('async: closing server')
+        server.close()
+      })
+    } catch (err) {
+      t.ifError(err)
+    }
+  })
+
+  await t.completed
+})
